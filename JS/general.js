b920ec6974739beb01e4572770cac1aa8b5e906e
Noel Moriano <mariano260996@gmail.com>
2019-05-12 09:55:14 -0500

    add navbar mobile, button floating

--- JS/general.js
+++ JS/general.js
@@ -13,3 +13,21 @@ document.addEventListener('DOMContentLoaded', function () {
         autoplay:true
     });
 });
+
+
+/********** NAVAR MOBILE ***********/
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+});
+/*********** img modal expande *************/
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, {});
+});
+
+/******** BUTTON FLOATING ***********/
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {});
+});
