380fe5ee26c97b23f89dc60fd61695bed7556556
Noel Moriano <mariano260996@gmail.com>
2019-05-15 00:11:42 -0500

    consumiendo json en home

---
+++ js/functions_data.js
@@ -0,0 +1,28 @@
+/*********** data GET ***********/
+function pasarVariables(pagina, nombres) {
+    pagina += "?";
+    nomVec = nombres.split(",");
+    for (i = 0; i < nomVec.length; i++)
+        pagina += nomVec[i] + "=" + escape(eval(nomVec[i])) + "&";
+    pagina = pagina.substring(0, pagina.length - 1);
+    location.href = pagina;
+}
+
+/************** FUNCTIONS DATA ***************/
+products_carousel = () => {
+    for (let i = 0; i <= 16; i++) {
+        document.write("<a class='carousel-item' id='carousel-item' href='#one!'><img src='img/productos/" + jsonProducts.products[i].img + ".jpg'><h6>" + jsonProducts.products[i].title + "</h6></a>");
+    }
+};
+
+list_products = () => {
+    for (let i = 0; i <= 16; i++) {
+        document.write("<li><a href='productos/product_detail/" + jsonProducts.products[i].id + "'>" + jsonProducts.products[i].title + "</a></li>");
+    }
+};
+
+product_general = () => {
+    for (i = 0; i <= 16; i++) {
+        document.write("<div class='item item-1'><div class='card-content white-text'><div><img src='img/productos/" + jsonProducts.products[i].img + ".jpg' width='100%' alt='' class='materialboxed'data-caption='" + jsonProducts.products[i].title + "'> </div> <a href='productos/product_detail'>" + jsonProducts.products[i].title + "</a> </div> </div>");
+    }
+};
