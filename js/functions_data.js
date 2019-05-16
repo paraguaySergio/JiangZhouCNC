9d4925cb1a974f1b663bf41d1981a79336c59571
Noel Moriano <mariano260996@gmail.com>
2019-05-16 01:10:25 -0500

    update web joungzhou

--- js/functions_data.js
+++ js/functions_data.js
@@ -8,6 +8,7 @@ function pasarVariables(pagina, nombres) {
     location.href = pagina;
 }
 
+/******************************************** SCRIPT HOME ****************************************************/
 /************** FUNCTIONS DATA ***************/
 products_carousel = () => {
     for (let i = 0; i <= 16; i++) {
@@ -17,12 +18,26 @@ products_carousel = () => {
 
 list_products = () => {
     for (let i = 0; i <= 16; i++) {
-        document.write("<li><a href='productos/product_detail/" + jsonProducts.products[i].id + "'>" + jsonProducts.products[i].title + "</a></li>");
+        document.write("<li><a href='productos/product_detail/" + jsonProducts.products[i].id + "' style='color:#fff;'>" + jsonProducts.products[i].title + "</a></li>");
     }
 };
 
 product_general = () => {
     for (i = 0; i <= 16; i++) {
-        document.write("<div class='item item-1'><div class='card-content white-text'><div><img src='img/productos/" + jsonProducts.products[i].img + ".jpg' width='100%' alt='' class='materialboxed'data-caption='" + jsonProducts.products[i].title + "'> </div> <a href='productos/product_detail'>" + jsonProducts.products[i].title + "</a> </div> </div>");
+        document.write("<div class='item item-1'><div class='card-content white-text'><div><img src='img/productos/" + jsonProducts.products[i].img + ".jpg' width='100%' alt=''data-caption='" + jsonProducts.products[i].title + "'> </div> <a href='productos/product_detail'>" + jsonProducts.products[i].title + "</a> </div> </div>");
+    }
+};
+
+
+/******************************************** SCRIPT PAGE PRODUCTS ****************************************************/
+page_product_list_products = () => {
+    for (let i = 0; i <= 16; i++) {
+        document.write("<li><a href='product_detail/" + jsonProducts.products[i].id + "'  style='color:#fff;'>" + jsonProducts.products[i].title + "</a></li>");
+    }
+};
+
+page_product_general = () => {
+    for (i = 0; i <= 16; i++) {
+        document.write("<div class='item item-1'><div class='card-content white-text'><div><img src='../img/productos/" + jsonProducts.products[i].img + ".jpg' width='100%' alt='' data-caption='" + jsonProducts.products[i].title + "'> </div> <a href='product_detail'>" + jsonProducts.products[i].title + "</a> </div> </div>");
     }
 };
