551a05c2b35c1ca1adc3040dfab29cf5ec089bd1
Noel Moriano <mariano260996@gmail.com>
2019-05-19 00:51:15 -0500

    optimization desing web and funcionality

--- js/home.js
+++ js/home.js
@@ -8,12 +8,12 @@ products_carousel = () => {
 
 list_products = () => {
     for (let i = 0; i <= 16; i++) {
-        document.write("<li><a href='productos/product_detail/?prodId=" + jsonProducts.products[i].id + "' style='color:#fff;'>" + jsonProducts.products[i].title + "</a></li>");
+        document.write("<li><a href='productos/product_detail/?prodId=" + jsonProducts.products[i].id + "'>" + jsonProducts.products[i].title + "</a></li>");
     }
 };
 
 product_general = () => {
     for (i = 0; i <= 16; i++) {
-        document.write("<div class='item item-1'><div class='card-content white-text'><div><img src='img/productos/" + jsonProducts.products[i].img + ".jpg' width='100%' alt=''data-caption='" + jsonProducts.products[i].title + "'> </div> <a href='productos/product_detail/?prodId=" + jsonProducts.products[i].id + "'>" + jsonProducts.products[i].title + "</a> </div> </div>");
+        document.write("<div class='item item-1'><div class='card-content white-text'><div><img src='img/productos/" + jsonProducts.products[i].img + ".jpg' width='100%' alt=''data-caption='" + jsonProducts.products[i].title + "'> </div> <a href='productos/product_detail/?prodId=" + jsonProducts.products[i].id + "'>" + jsonProducts.products[i].title + "</a><span class='div-button'><a  href='productos/product_detail/?prodId=" + jsonProducts.products[i].id + "'>Cotizar</a></span></div></div>");
     }
 };
