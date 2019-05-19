aa960b815806fc70044e65a57d774003b8141251
palaciosAlex <davixdmarzo97@gmail.com>
2019-05-19 18:22:53 -0500

    el blog

---
+++ js/blog_data.js
@@ -0,0 +1,21 @@
+let jsonBlog = {
+    "blog": [
+        {
+            "id": 0,
+            "title": "Ventajas de las maquinas de corte laser",
+            "description": "Las máquinas cortadoras láser; permiten cortar, como su nombre lo indica, muchos materiales de diversas densidades con un acabado perfecto y una precisión milimétrica imposible de igualar, en comparación real con otro tipo de maquinaria diseñada para este fin.",
+            "img": "DK7716CNCWIRECUTEDM",
+        },
+        {
+            "id": 1,
+            "title": "D71 DIE SINKER EDM",
+            "description": "D71 DIE SINKER EDM",
+            "img": "D71DIESINKEREDM",
+        }, {
+            "id": 2,
+            "title": "JZDX SERIES CNC CARVE-MILLING MACHINE",
+            "description": "JZDX SERIES CNC CARVE-MILLING MACHINE",
+            "img": "JZDXSERIESCNCCARVE-MILLINGMACHINE",
+        }
+    ]
+};
