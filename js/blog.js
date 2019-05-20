5b5b1ab2604d6cfe636227bdc05debdf103d9833
Noel Moriano <mariano260996@gmail.com>
2019-05-20 00:34:39 -0500

    contend blog

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,11 @@
-import "blog_data";
+let dataBlog = jsonBlog.blog;
+let numPosts = jsonBlog.blog.length;
 
-page_blog_general = () => {
+let contentBlog = document.getElementById("contenido-post");
 
-    let postId = document.getElementById("contenido-post").innerHTML = "hola";
+for (let i = 0; i <= numPosts; i++) {
+    let contentBlog = "<div class='img-post'><img src='../img/productos/" + dataBlog[i].img + ".jpg' alt=''></div><div class='description-post'><h5>" + dataBlog[i].title + "</h5><p>" + dataBlog[i].description + "</p><a class='waves-effect waves-light btn modal-trigger' href='#modal1' onclick='leerMas()'>Leer Mas</a></div>";
+}
 
-};
+contentBlog.innerHTML;
+
