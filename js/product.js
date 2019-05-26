942915120e56eac87eb884345d0c765d65085708
Noel Moriano <mariano260996@gmail.com>
2019-05-26 01:16:28 -0500

    Correccion de bugs , se agrego Grid de regillas, nuevos rediseño de forms , title corregidos, cambios realizados en todas las ventanas

--- js/product.js
+++ js/product.js
@@ -1,12 +1,14 @@
 /******************************************** SCRIPT PAGE PRODUCTS ****************************************************/
 page_product_list_products = () => {
-    for (let i = 0; i <= 16; i++) {
+    let cantLP = jsonProducts.products.length;
+    for (let i = 0; i <= cantLP; i++) {
         document.write("<li><a href='product_detail/?prodId=" + jsonProducts.products[i].id + "'  style='color:#fff;'>" + jsonProducts.products[i].title + "</a></li>");
     }
 };
 
 page_product_general = () => {
-    for (i = 0; i <= 16; i++) {
+    let cantPG = jsonProducts.products.length;
+    for (i = 0; i <= cantPG; i++) {
         document.write("<div class='item item-1'><div class='card-content white-text'><div><img src='../img/productos/" + jsonProducts.products[i].img + ".jpg' width='100%' alt='' data-caption='" + jsonProducts.products[i].title + "'> </div> <a href='product_detail/?prodId=" + jsonProducts.products[i].id + "'>" + jsonProducts.products[i].title + "</a><span class='div-button'><a  href='product_detail/?prodId=" + jsonProducts.products[i].id + "'>Cotizar</a></span></div> </div>");
     }
 };
